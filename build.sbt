1a0a38ab58887daeeb922d3feeb3eddf4b6cd3b1
Arthur Lui <luiarthur@gmail.com>
2016-09-20 17:55:21 -0700

    yes

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "hearts"
+
+version := "v0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+)
---
+++ src/test/scala/TestSuite.scala
@@ -0,0 +1,12 @@
+import org.scalatest.FunSuite
+
+class TestSuite extends FunSuite {
+  def round(x: Double, d: Int = 2) = 
+    (scala.math.pow(10,d) * x).toInt / scala.math.pow(10,d)
+
+  test("Just a test") {
+    import ORF.models._
+    assert(true)
+  }
+
+}
