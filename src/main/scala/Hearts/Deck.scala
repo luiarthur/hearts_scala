8d0bb368e2373da623a31ab3ae407226cef6ebc0
Arthur Lui <luiarthur@gmail.com>
2016-09-21 07:32:04 -0700

    yes

--- src/main/scala/Hearts/Deck.scala
+++ src/main/scala/Hearts/Deck.scala
@@ -1,9 +1,16 @@
 package Hearts
 
-class Cards {
+class Deck(val seed: Int = 0)  {
+  scala.util.Random.setSeed(0)
+  val Rand = scala.util.Random
+
   val suits = List("S","H","C","D") // spade, heart, club, diamond
   val values = (2 to 10).toList.map(_.toString) ++ List("J","Q","K","A")
   
   val cards = 
     for (suit <- suits; value <- values) yield value + suit
+
+  def shuffle = Rand.shuffle(cards)
+
+  def deal = 
 }
